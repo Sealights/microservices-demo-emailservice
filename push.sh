1eccf14af5a192fb1573729200b12978a5a36688
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-19 17:27:49 +0200

    Added emailservice

---
+++ push.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+
+docker build -t microservices-demo-emailservice .
+docker tag microservices-demo-emailservice:latest 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-emailservice:latest
+docker push 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-emailservice:latest
