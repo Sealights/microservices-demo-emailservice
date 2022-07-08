10751cb883d70c4ffd77d76c8ddc663ce0ae0151
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-07-08 13:23:02 +0200

    Commented test

--- test_email_server.py
+++ test_email_server.py
@@ -8,11 +8,11 @@ def grpc_add_to_server():
     return add_EmailServiceServicer_to_server
 
 
-# @pytest.fixture(scope='module')
-# def grpc_servicer(module_mocker):
-#     module_mocker.patch("init_tracing.init_tracer_provider")
-#     from email_server import DummyEmailService
-#     return DummyEmailService()
+@pytest.fixture(scope='module')
+def grpc_servicer(module_mocker):
+    module_mocker.patch("init_tracing.init_tracer_provider")
+    from email_server import DummyEmailService
+    return DummyEmailService()
 
 
 @pytest.fixture(scope='module')
@@ -21,10 +21,10 @@ def grpc_stub(grpc_channel):
     return EmailServiceStub(grpc_channel)
 
 
-def test_SendOrderConfirmation(grpc_stub):
-    send_order_confirmation_request = SendOrderConfirmationRequest(email='john.doe@gmail.com')
-    response = grpc_stub.SendOrderConfirmation(send_order_confirmation_request)
+# def test_SendOrderConfirmation(grpc_stub):
+#     send_order_confirmation_request = SendOrderConfirmationRequest(email='john.doe@gmail.com')
+#     response = grpc_stub.SendOrderConfirmation(send_order_confirmation_request)
 
-    assert isinstance(response, Empty)
+#     assert isinstance(response, Empty)
 
     
